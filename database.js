b532b6b840e2bd16a0e7b0ce612d932bd91650fc
Akinrolabu Lolade <l.akinrolabu@csdcconsulting.com>
2020-04-13 20:29:34 +0100

    Added function

--- database.js
+++ database.js
@@ -1,15 +1,34 @@
-var mongodb = require('mongodb');
-var mongoClient = require('mongodb').MongoClient;
-
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+const insertDocuments = require('./interns');
 var url = "mongodb://localhost:27017/lodakins";
 
-mongoClient.connect(url,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },function(err,db){
-    if(err) throw err;
-
-    console.log("database created by lodakins");
-
-    db.close();
+var mongo = new MongoClient(url,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+mongo.connect(function(err,db){
+   assert.equal(err,null);
+    console.log("database created successfully");
+      //const collect = db.collection("myMovies");
+     var dbo = db.db('lodakins');
+      insertDocuments(dbo,(result)=>{
+       console.log(result);
+        
+       db.close();
+   })
+})
+
+
+
+// mongoClient.connect(url,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   },function(err,db){
+//     if(err) throw err;
+
+//     console.log("database created by lodakins");
+
+//     db.close();
+// });
